0147527d7e27130dda2fa4ce4eba16b08d397069
Paul Leber <luaprebel@gmail.com>
2024-06-15 08:39:02 -0700

    Add home screen and seed screen

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.screen import Screen
+from textual.widgets import Footer, Header, Markdown
 
-from seeds import SeedsTab
+from seeds import SeedsScreen
+
+
+with open("README.md") as file:
+    MARKDOWN = file.read()
+
+
+class HomeScreen(Screen):
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Markdown(markdown=MARKDOWN)
+        yield Footer()
 
 
 class PyGardener(App):
 
+    CSS_PATH = "app.tcss"
+    SCREENS = {"home": HomeScreen(), "seeds": SeedsScreen()}
     BINDINGS = [
+        ("h", "push_screen('home')", "Home"),
+        ("s", "push_screen('seeds')", "Seeds"),
         ("d", "toggle_dark", "Toggle Dark Mode"),
         ("q", "quit", "Quit"),
     ]
@@ -14,7 +31,9 @@ class PyGardener(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield SeedsTab()
+
+    def on_mount(self) -> None:
+        self.push_screen(HomeScreen())
 
     def action_quit(self) -> None:
         self.exit()
--- seeds.py
+++ seeds.py
@@ -1,18 +1,24 @@
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widget import Widget
-from textual.widgets import DataTable
+from textual.widgets import DataTable, Footer, Header
 
 from data import SEEDS
 
 
-class SeedsTab(Widget):
+class SeedsScreen(Screen):
     """Widget to manage seed collections."""
 
+    BINDINGS = [("a", "add_seed", "Add Seed")]
+
     def compose(self) -> ComposeResult:
-        yield DataTable()
+        yield Header()
+        yield DataTable(id="seed-table")
+        yield Footer()
 
     def on_mount(self) -> None:
         table = self.query_one(DataTable)
         table.add_columns(*SEEDS[0])
         table.add_rows(SEEDS[1:])
+
+    def action_add_seed(self) -> None:
+        pass
